4b42e0f021810ed3164e34ef522697979f278a8e
Asadbek Xushnayev <xushnaevasadbek@gmail.com>
2025-08-24 18:44:01 +0500

    Array

--- Program.cs
+++ Program.cs
@@ -26,5 +26,42 @@ string[] days = { "Dushanba", "Seshanba", "Chorshanba", "Payshanba", "Juma", "Sh
 
 foreach(string day in days)
 {
-    Console.WriteLine(day);
+    //Console.WriteLine(day);
+}
+
+
+int drinkId = 2;
+
+while(drinkId<1 || drinkId > 4)
+{
+    //Console.WriteLine("Ichimlik tanlang:");
+    //Console.WriteLine("1: Cola ; 2: Fanta ; 3: Sprite ; 4: Aqua");
+    drinkId = int.Parse(Console.ReadLine());
+
+}
+
+Console.WriteLine($"Siz {drinkId} - ichimlikni tanladingiz");
+
+while (true)
+{
+    //Console.WriteLine(DateTime.Now.Second);
+
+    if (DateTime.Now.Second % 5 == 0)
+        break;
+}
+//long A , B ;
+
+//A = long.Parse(Console.ReadLine());
+//B = long.Parse(Console.ReadLine());
+//long c = A + B;
+//Console.WriteLine(c);
+
+int rows = 10, column = 10;
+for (int a = 1; a <= rows; a++)
+{
+    for(int b = 1 ; b <= column; b++)
+    {
+        Console.Write($"{b}*{a} = {(b*a)}\t");
+    }
+    Console.WriteLine();
 }
